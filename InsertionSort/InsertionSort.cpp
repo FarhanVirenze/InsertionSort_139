bca4ec69bace803233e29936c9875b8ae283c357
FarhanVirenze <farhanvirenze18@gmail.com>
2023-03-23 17:33:58 +0700

    tambah void display

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -66,6 +66,14 @@ void insertionsort()
     }
 };
 
+void display()
+{
+    cout << endl;                       // output baris kosong
+    cout << "total element movement= " << n - 1 << endl;  // cout movement element
+    cout << "\n==================" << endl;      // output ke layar
+    cout << "element array yang telah tersusun" << endl; // output ke layar
+    cout << "===================" << endl; // output ke layar
+
 
 int main()
 {
